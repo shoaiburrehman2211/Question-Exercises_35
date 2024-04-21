98981dad60101f959573f63b2e65ebbdd99ed4c7
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 04:46:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+Think of at least three different animals that have a common characteristic. Store the names
+of these animals in a list, and then use a for loop to print out the name of each animal.
+• Modify your program to print a statement about each animal, such as A dog would make a great pet.
+• Add a line at the end of your program stating what these animals have in common. You could print
+a sentence such as Any of these animals would make a great pet!
+*/
+var animalsName = ["Cow", "Sheep", "Goat", "Camel"];
+// priting only name
+for (var _i = 0, animalsName_1 = animalsName; _i < animalsName_1.length; _i++) {
+    var i = animalsName_1[_i];
+    console.log(i);
+}
+for (var _a = 0, animalsName_2 = animalsName; _a < animalsName_2.length; _a++) {
+    var j = animalsName_2[_a];
+    console.log("".concat(j, " is a Domestic Animal"));
+}
+console.log("All thse animals' ".concat(animalsName[0], ", ").concat(animalsName[1], ", $\n{animalsName[2]} and ").concat(animalsName[3], " meat is Halal in Islam"));
---
+++ main.ts
@@ -0,0 +1,23 @@
+/*
+Think of at least three different animals that have a common characteristic. Store the names 
+of these animals in a list, and then use a for loop to print out the name of each animal.
+• Modify your program to print a statement about each animal, such as A dog would make a great pet.
+• Add a line at the end of your program stating what these animals have in common. You could print 
+a sentence such as Any of these animals would make a great pet!
+*/
+
+
+let animalsName = ["Cow", "Sheep", "Goat", "Camel"];
+
+// priting only name
+
+for(let i of animalsName){
+console.log(i);
+}
+
+for(let j of animalsName){
+    console.log(`${j} is a Domestic Animal`);
+}
+
+console.log(`All thse animals' ${animalsName[0]}, ${animalsName[1]}, $
+{animalsName[2]} and ${animalsName[3]} meat is Halal in Islam`);
